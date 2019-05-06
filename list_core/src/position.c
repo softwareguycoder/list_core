ea73f68c15c3a17a0435888912be0345c65e50fa
softwareguycoder <softwareguycoder@gmail.com>
2019-05-06 09:58:59 -0400

    Added GNU General Public License to position.c

--- list_core/src/position.c
+++ list_core/src/position.c
@@ -1,6 +1,21 @@
 // position.c - Contains implementations of functions that manipulate
 // information in instances of POSITION structures
 //
+// This file is part of list_core.
+//
+// list_core is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// list_core is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with list_core.  If not, see <https://www.gnu.org/licenses/>.
+//
 
 #include "stdafx.h"
 #include "list_core.h"
