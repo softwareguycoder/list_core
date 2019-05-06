31c8c7aa817b0688e551660f37ca10a4419f2234
softwareguycoder <softwareguycoder@gmail.com>
2019-05-06 15:15:23 -0400

    Reformatted position.c and move MoveToTailPosition to alpha order

--- list_core/src/position.c
+++ list_core/src/position.c
@@ -26,120 +26,118 @@
 // Internal-use-only functions
 
 LPPOSITION GetNext(LPPOSITION pos) {
-  if (pos == NULL) {
-    return NULL;  // Required parameter
-  }
+	if (pos == NULL) {
+		return NULL;  // Required parameter
+	}
 
-  return pos->pNext;
+	return pos->pNext;
 }
 
 LPPOSITION GetPrev(LPPOSITION pos) {
-  if (pos == NULL) {
-    return NULL;
-  }
-  return pos->pPrev;
+	if (pos == NULL) {
+		return NULL;
+	}
+	return pos->pPrev;
 }
 
 //////////////////////////////////////////////////////////////////////////////
 // Publicly-exposed functions
 
 void CreatePosition(LPPPOSITION lppPosition) {
-  if (lppPosition == NULL) {
-    return;
-  }
+	if (lppPosition == NULL) {
+		return;
+	}
 
-  *lppPosition = (LPPOSITION)malloc(sizeof(POSITION));
-  memset(*lppPosition, 0, sizeof(POSITION));
+	*lppPosition = (LPPOSITION) malloc(sizeof(POSITION));
+	memset(*lppPosition, 0, sizeof(POSITION));
 }
 
 void DestroyPosition(LPPPOSITION lppPosition) {
-  if (lppPosition == NULL || *lppPosition == NULL) {
-    return;
-  }
+	if (lppPosition == NULL || *lppPosition == NULL) {
+		return;
+	}
 
-  free((void*)(*lppPosition));
-  *lppPosition = NULL;
+	free((void*) (*lppPosition));
+	*lppPosition = NULL;
 }
 
 LPPOSITION GetNextPosition(LPPOSITION lpElement) {
-  if (lpElement == NULL) {
-    return NULL;
-  }
-  return lpElement->pNext;
+	if (lpElement == NULL) {
+		return NULL;
+	}
+	return lpElement->pNext;
 }
 
 LPPOSITION GetPrevPosition(LPPOSITION lpElement) {
-  if (lpElement == NULL) {
-    return NULL;
-  }
-  return lpElement->pPrev;
-}
-
-void MoveToTailPosition(LPPPOSITION lppElement) {
-  if (lppElement == NULL || *lppElement == NULL) {
-    return;  // nothing in the list to do anything with
-  }
-
-  do {
-    /* If we are already given a position that points
-       to the tail, there is nothing to do but return
-       the same thing as we were given. */
-    if (IsPositionTail(*lppElement)) {
-      return;
-    }
-  } while((*lppElement = GetNext(*lppElement)) != NULL);
+	if (lpElement == NULL) {
+		return NULL;
+	}
+	return lpElement->pPrev;
 }
 
 BOOL IsPositionHead(LPPOSITION lpElement) {
-  if (lpElement == NULL) {
-    return FALSE;
-  }
-  return lpElement->pPrev == NULL;
+	if (lpElement == NULL) {
+		return FALSE;
+	}
+	return lpElement->pPrev == NULL;
 }
 
 BOOL IsPositionTail(LPPOSITION lpElement) {
-  if (lpElement == NULL) {
-    return FALSE;
-  }
-  return lpElement->pNext == NULL;
+	if (lpElement == NULL) {
+		return FALSE;
+	}
+	return lpElement->pNext == NULL;
 }
 
 BOOL IsSoleElement(LPPOSITION lpElement) {
-  if (lpElement == NULL) {
-    return FALSE;
-  }
+	if (lpElement == NULL) {
+		return FALSE;
+	}
 
-  return lpElement->pPrev == NULL
-      && lpElement->pNext == NULL;
+	return lpElement->pPrev == NULL && lpElement->pNext == NULL;
 }
 
-
 void MoveToHeadPosition(LPPPOSITION lppElement) {
-  if (lppElement == NULL || *lppElement == NULL) {
-    return;  // nothing in the list to do anything with
-  }
+	if (lppElement == NULL || *lppElement == NULL) {
+		return;  // nothing in the list to do anything with
+	}
 
-  do {
-    /* If we are already given a position that points
-       to the head, there is nothing to do but return
-       the same thing as we were given. */
-    if (IsPositionHead(*lppElement)) {
-      return;
-    }
-  } while((*lppElement = GetPrev(*lppElement)) != NULL);
+	do {
+		/* If we are already given a position that points
+		 to the head, there is nothing to do but return
+		 the same thing as we were given. */
+		if (IsPositionHead(*lppElement)) {
+			return;
+		}
+	} while ((*lppElement = GetPrev(*lppElement)) != NULL);
+}
+
+void MoveToTailPosition(LPPPOSITION lppElement) {
+	if (lppElement == NULL || *lppElement == NULL) {
+		return;  // nothing in the list to do anything with
+	}
+
+	do {
+		/* If we are already given a position that points
+		 to the tail, there is nothing to do but return
+		 the same thing as we were given. */
+		if (IsPositionTail(*lppElement)) {
+			return;
+		}
+	} while ((*lppElement = GetNext(*lppElement)) != NULL);
 }
 
 void SetNextPosition(LPPOSITION lpElement, LPPOSITION lpValue) {
-  lpElement->pNext = lpValue;
+	lpElement->pNext = lpValue;
 }
 
 void SetPositionData(LPPOSITION lpElement, void* pvData) {
-  if (lpElement == NULL) {
-    return; // Required parameter
-  }
-  lpElement->pvData = pvData;
+	if (lpElement == NULL) {
+		return; // Required parameter
+	}
+	lpElement->pvData = pvData;
 }
 
 void SetPrevPosition(LPPOSITION lpElement, LPPOSITION lpValue) {
-  lpElement->pPrev = lpValue;
+	lpElement->pPrev = lpValue;
 }
